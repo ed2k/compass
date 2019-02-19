6afdb4ba6c8dba1cf975709d5bed433c443d97c3
Your Name <you@example.com>
2019-02-19 00:56:31 +0000

    reduce docker hub repo name

--- docs/private_tangle/01_calculate_layers.sh
+++ docs/private_tangle/01_calculate_layers.sh
@@ -5,4 +5,4 @@ scriptdir=$(dirname "$(readlink -f "$0")")
 
 #load_config
 echo hello $1 $2 $3
-docker run -t --rm -v $scriptdir/data:/data ed2k/test/docker:layers_calculator layers_calculator_deploy.jar -sigMode CURLP27 -seed $1 -depth 11 -security 1 -layers /data/layers -lstart $2 -lcount $3
+docker run -t --rm -v $scriptdir/data:/data ed2k/docker:layers_calculator layers_calculator_deploy.jar -sigMode CURLP27 -seed $1 -depth 11 -security 1 -layers /data/layers -lstart $2 -lcount $3
